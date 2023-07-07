0c3533cc669227bcd380d619f42182586b7727a2
Cuervo <joaquinlevis2004@gmail.com>
2023-07-07 19:50:50 -0300

    comienzo de act 7 para entregar

--- actividades/actividad7_JoaquinLevis.c
+++ actividades/actividad7_JoaquinLevis.c
@@ -12,17 +12,31 @@ typedef struct
 Tarreglo arregloBi;
 
 void cargar(Tarreglo *matriz){
-    for(int i = 0;i<F;i++){
-        for (int j = 0; j < C; j++){
+    for(int i = 0;i<(*matriz).n;i++){
+        for (int j = 0; j < (*matriz).m; j++){
             printf("Ingrese fila %d columna %d:", i+1,j+1);
-            scanf("%d", &matriz[i][j]);
+            scanf("%d", &(matriz->arr[i][j]));
+        }   
+    }
+}
+
+void mostrar(Tarreglo *vBi){
+    for(int i = 0;i<(*vBi).n;i++){
+        for (int j = 0; j < (*vBi).m; j++){
+            printf("En la fila %d columna %d hay: ", i+1,j+1);
+            printf("%d\n", vBi->arr[i][j]);
         }   
     }
 }
 
 
 int main(){
-    cargar
+    printf("Ingrese la cantidad de n filas que tendra el arreglo bidimensional");
+    scanf("%d",arregloBi.n);
+    printf("Ingrese la cantidad de m columnas que tendra el arreglo bidimensional");
+    scanf("%d",arregloBi.m);
+    cargar(&arregloBi);
+    mostrar(&arregloBi);
 
 
 
